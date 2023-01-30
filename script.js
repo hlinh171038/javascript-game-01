806e56ba2bb273dd6893b0643d4bd874b36ffe43
HL <hoanglinh171038@gmail.com>
2023-01-30 15:31:46 +0700

    complete function without css

--- script.js
+++ script.js
@@ -8,6 +8,35 @@ let userChoice ;
  userPlayerChoice.forEach(userPlayerChoice => userPlayerChoice.addEventListener('click', (e) => {
  userChoice = e.target.id;
  userPlayerDisplay.innerHTML = userChoice;
+ randomComputerChoice();
+ resultAll();
 }))
-
-
+let computerChoice;
+function randomComputerChoice () {
+    let random = Math.floor(Math.random()*3)+ 1;
+    
+   if (random == 1 ){
+    computerChoice = 'rock'
+   }
+   if (random == 2 ){
+    computerChoice = 'paper'
+   }
+   if (random == 3 ){
+    computerChoice = 'scissor'
+   }
+   computerPlayerDisplay.innerHTML = computerChoice;
+}
+let forAll;
+function resultAll(){
+    if(userChoice == computerChoice){
+        forAll = "draw";
+    }
+    else if ((userChoice == 'scissor' && computerChoice == 'rock') || 
+    (userChoice == 'rock' && computerChoice == 'paper') || 
+    (userChoice == 'paper'&& computerChoice == 'scissor')){
+        forAll ='loser';
+    }else {
+        forAll = 'you win';
+    }
+    result.innerHTML = forAll;
+}
